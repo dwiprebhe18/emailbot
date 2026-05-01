14fb4af2d8fe9de053fca41191c627248dbc54cb
dwiprebhe18 <rebhedwip6@gmail.com>
2026-05-01 01:05:38 -0700

    clean App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
-import LoginSignupPage from './pages/LoginSignupPage'
-import TemplatePage from './pages/TemplatePage'
-import DashboardPage from './pages/DashboardPage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginSignupPage />} />
-      <Route path="/templates" element={<TemplatePage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
     </Routes>
   )
 }
