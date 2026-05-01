b38b73507330bd12ceafa000b9dd8c72e94cab1c
dwiprebhe18 <rebhedwip6@gmail.com>
2026-05-01 01:16:26 -0700

    update App.jsx with real routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import TemplatePage from './pages/TemplatePage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/templates" element={<TemplatePage />} />
     </Routes>
   )
 }
